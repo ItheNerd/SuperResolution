fe04ce33f4fbee772a1ef938e4f7db327305daff
Jay <99102582+ItheNerd@users.noreply.github.com>
2023-03-25 22:39:25 +0530

    added react frontend

---
+++ backend/main.py
@@ -0,0 +1,36 @@
+import cv2
+import numpy as np
+import io
+import pickle
+from fastapi import FastAPI, File, UploadFile
+from starlette.responses import StreamingResponse
+
+# Load the pickled model from a file
+with open("realesrgan_x4plus_model.pkl", "rb") as f:
+    model = pickle.load(f)
+
+app = FastAPI()
+
+
+@app.post("/enhance_image")
+async def enhance_image(image: UploadFile = File(...)):
+    # Read the image file and convert it to a NumPy array
+    file_contents = await image.read()
+    nparr = np.fromstring(file_contents, np.uint8)
+    input_image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    print("Input Recieved")
+    # Enhance the input image using the RealESRGAN model
+    output_image, _ = model.enhance(input_image, outscale=4)
+    cv2.imwrite("output.png", output_image)
+
+    print("Output generated")
+    # Encode the output image as PNG binary data
+    success, encoded_image = cv2.imencode(".png ", output_image)
+    if not success:
+        return {"message": "Failed to encode image"}
+
+    # Return the enhanced image as a file download
+    print("Output sent")
+    return StreamingResponse(
+        io.BytesIO(encoded_image.tobytes()), media_type="image/png"
+    )
---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import DropZone from "./components/dropZone3.jsx";
+
+function App() {
+  return (
+    <div>
+      <DropZone />
+    </div>
+  );
+}
+
+export default App;
